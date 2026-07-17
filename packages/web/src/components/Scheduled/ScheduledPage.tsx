282c971792ca62c997e6f56676ddf1471905b50f
laughingTT <tianjian361@163.com>
2026-07-17 10:53:39 +0800

    chore: auto-save /data/projects/datanova/packages/web/src/components/Scheduled/ScheduledPage.tsx

--- packages/web/src/components/Scheduled/ScheduledPage.tsx
+++ packages/web/src/components/Scheduled/ScheduledPage.tsx
@@ -273,6 +273,7 @@ export default function ScheduledPage() {
                     )}
                   </div>
                 )}
+                </div>
               </div>
             ))}
           </div>
